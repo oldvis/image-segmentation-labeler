ea54cfe51ecfd8e5eb0fa721fb31d6084aa200ff
Yu Zhang <yuzhang94@outlook.com>
2023-11-28 23:05:27 +0800

    refactor: use setup

--- src/components/VDataEntry.vue
+++ src/components/VDataEntry.vue
@@ -1,31 +1,26 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import type { PropType } from 'vue'
 import { dataTypeImage } from '@onelabeler/core'
 import type { DataObject } from '~/stores/annotation/types'
 import { BaseOverlay } from '~/packages/label-task-types/chart'
 
-export default defineComponent({
-  components: {
-    BaseDisplay: dataTypeImage.BaseDisplay,
-    BaseOverlay,
+const props = defineProps({
+  datum: {
+    type: Object as PropType<DataObject>,
+    required: true,
   },
-  props: {
-    datum: {
-      type: Object as PropType<DataObject>,
-      required: true,
-    },
-    index: {
-      type: Number as PropType<number | null>,
-      default: null,
-    },
-  },
-  computed: {
-    filename(): string | undefined {
-      return (this.datum.value as { filename?: string })?.filename
-    },
+  index: {
+    type: Number as PropType<number | null>,
+    default: null,
   },
 })
+
+const { datum } = toRefs(props)
+const { BaseDisplay } = dataTypeImage
+
+const filename = computed(() => (
+  (datum.value as { filename?: string })?.filename
+))
 </script>
 
 <template>
@@ -46,9 +41,9 @@ export default defineComponent({
         class="flex-1"
         :data-object="datum"
       >
-        <template #overlay="props">
+        <template #overlay="overlayProps">
           <BaseOverlay
-            v-bind="props"
+            v-bind="overlayProps"
             :data-object="datum"
             style="grid-area: 1 / 1 / 2 / 2"
           />
