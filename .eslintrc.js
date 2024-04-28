c578c3095c56736efd6d31db9eac0a7820e7cbd0
Yu Zhang <yuzhang94@outlook.com>
2024-04-28 10:29:19 +0800

    build: update dependencies

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@antfu'],
-  rules: {
-    'arrow-parens': ['error', 'always'],
-    'curly': ['error', 'multi-line'],
-    'antfu/if-newline': ['off'],
-    'antfu/top-level-function': ['off'],
-  },
-}
